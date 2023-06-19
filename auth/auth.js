68cf0f6fb733a40f3d98c17a50762beb68bdc13a
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-06-19 21:05:26 +0530

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -5,7 +5,7 @@ const authantication = async(req,res,next) =>{
         const tokens = req.cookies.jwtToken;
         const verifyToken = jwt.verify(tokens,"MIHIR")
         const getRootUser = await register.findOne({_id:verifyToken._id,"tokens.token":tokens})
-      if(!getRootUser) {throw new Error('User Not found')} 
+      if(!getRootUser) {res.json({message:"user not found"})} 
       req.tokens = tokens;
       req.rootUser = getRootUser;
       req.userId = getRootUser._id;      
